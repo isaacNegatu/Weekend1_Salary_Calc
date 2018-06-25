7061f1f3270c57c36e0f19196c0f73800ab4a059
Isaac Negatu <isaac.negatu@gmail.com>
2018-06-25 09:36:23 -0500

    formatted html and js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(start);
 
-class Employee{
-  constructor(fName, lName, ID, title, salary){
+class Employee {
+  constructor(fName, lName, ID, title, salary) {
     this.fName = fName;
     this.lName = lName;
     this.id = ID;
@@ -9,53 +9,53 @@ class Employee{
     this.salary = salary;
   }
 
-  calculateMonthlyCost(){
-    return parseInt(this.salary)/12;
+  calculateMonthlyCost() {
+    return parseInt(this.salary) / 12;
   }
 
 }
 
 let employees = [];
 
-function start(){
+function start() {
   eventHandler();
 }
 
-function eventHandler(){
+function eventHandler() {
   $('.table').on('click', '.delete', deleteClicked);
   $('#submit').click(submitClicked);
 }
 
-function deleteClicked(){
+function deleteClicked() {
   employees = employees.filter(employee => employee.id != $(this).attr('id'));
   $(this).parentsUntil('tbody').remove();
   calculateCost();
 }
 
-function submitClicked(){
+function submitClicked() {
   let flag = true;
-  $('input').each(function(){
-    if($(this).val() == ''){
+  $('input').each(function() {
+    if ($(this).val() == '') {
       flag = false;
       $('#warning').text('You must fill in all the fields');
     }
   });
 
-  let findID = employees.find(function(employee){
+  let findID = employees.find(function(employee) {
     return employee.id == $('#ID').val();
   });
 
-  if(findID){
+  if (findID) {
     flag = false;
     $('#warning').text('Employee with the same ID exists');
   }
 
   let tableBody = $('.tableBody');
 
-  if(flag){
+  if (flag) {
     $('#warning').text('');
 
-    employees.push(new Employee($('#fName').val(),$('#lName').val(),$('#ID').val(),$('#title').val(),$('#salary').val()));
+    employees.push(new Employee($('#fName').val(), $('#lName').val(), $('#ID').val(), $('#title').val(), $('#salary').val()));
 
     let action = $(`<td><button id=${$('#ID').val()} class="delete"><i class="fa fa-close"></i></button></td>`);
     let firstName = $(`<td>${$('#fName').val()}</td>`);
@@ -77,16 +77,16 @@ function submitClicked(){
 }
 
 
-function calculateCost(){
+function calculateCost() {
   let monthlyCost = 0;
 
-  for(let i = 0; i < employees.length; i++){
-    monthlyCost+= employees[i].calculateMonthlyCost();
+  for (let i = 0; i < employees.length; i++) {
+    monthlyCost += employees[i].calculateMonthlyCost();
   }
 
-  if(monthlyCost <= 20000){
+  if (monthlyCost <= 20000) {
     $('#cost').text(monthlyCost);
-  }else{
+  } else {
     $('#cost').text(monthlyCost).css('background-color', 'rgba(255, 0, 0, 0.5)');
   }
 
